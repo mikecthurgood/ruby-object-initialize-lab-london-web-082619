79663fbb918cbda91d78677da3d6129ddfdd912f
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-28 08:00:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,5 +5,19 @@ class Dog
     @breed = breed
   end
     
+    def name=(name)
+      @name = name
+    end
+    
     def name
+      @name = name
+    end
+    
+    def breed=(breed)
+      @breed = breed
+    end
+    
+    def breed
+      @breed = breed
+    end
   end
